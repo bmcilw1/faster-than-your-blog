aed0bfd5632bd87fb3a3a713e33b2b97730ee1bd
Brian McIlwain <brianwmcilwain@gmail.com>
2019-03-01 19:00:38 -0800

    Added new page for blog

---
+++ webapp/src/views/Blog.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="blog">
+    <div class="blog-content">
+      <h1>Title of Blog</h1>
+    </div>
+    <div class="links">
+      <h2>Links</h2>
+    </div>
+    <div class="comments">
+      <h2>Comments</h2>
+    </div>
+  </div>
+</template>
